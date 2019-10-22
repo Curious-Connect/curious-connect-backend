8745db93f00c73c982b309c7a08901ca32e6bce8
Jennifer V Phan <jennyphan181295@gmail.com>
2019-10-23 00:17:43 +0200

    setting up nodemailer

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/send", (req, res, next) => {
+    const { name, email, message } = req.body;
+
+    let mail = {
+        from: email,
+        to: process.env.EMAIL,
+        subject: `Contact message from ${name}`,
+        html: message
+    }
+
+    transporter.sendMail(mail, (err, data) => {
+        if (err) {
+            res.json({
+                message: "Fail to send email"
+            })
+        } else {
+            res.json({
+                message: "Email sent."
+            })
+        }
+    })
+});
+
+module.exports = router;

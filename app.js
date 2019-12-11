e7f1a94c6a8eff998f424a37d68e23420d41c74d
Tatijana Rajcic <tatijana.rajcic@essec.edu>
2019-12-11 23:14:54 +0100

    added listenning to server

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 const index = require('./routes/index');
 app.use("/", index);
 
+app.listen(process.env.PORT, () => console.log("My Exhibition project is running"));
+
 app.use((req, res, next) => {
   // If no routes match, send them the React HTML.
   res.sendFile(__dirname + "/public/index.html");

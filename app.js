fa7121f4fc473790c64728d5eaa43af007832782
Tatijana Rajcic <tatijana.rajcic@essec.edu>
2019-12-11 23:36:33 +0100

    added static public

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express();
 const index = require('./routes/index');
 app.use("/", index);
 
-app.listen(process.env.PORT, () => console.log("My Exhibition project is running"));
+app.use(express.static("public"))
+
+app.listen(process.env.PORT, () => console.log("My project is running"));
 
 app.use((req, res, next) => {
   // If no routes match, send them the React HTML.

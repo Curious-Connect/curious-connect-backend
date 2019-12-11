dccd2f3a15e6999f32e4b69febb7b6f04f1f6944
Tatijana Rajcic <tatijana.rajcic@essec.edu>
2019-12-11 17:57:44 +0100

    removed get route

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-
-router.get("/", function(req,res) {
-    res.send("connected")
-})
-
 router.post("/send", (req, res, next) => {
     const { name, email, message } = req.body;
 

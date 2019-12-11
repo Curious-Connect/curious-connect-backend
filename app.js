a6302a6abbf3635e9ef9418231e677656ed81293
Tatijana Rajcic <tatijana.rajcic@essec.edu>
2019-12-11 17:17:45 +0100

    before deployment

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ const app = express();
 
 const index = require('./routes/index');
 app.use("/", index);
+
+app.use((req, res, next) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/public/index.html");
+});
---
+++ public/precache-manifest.50498bc058ae3142a53f134c7419444b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d56ae3e9e00a9a84381eac097abe683",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efc7751b88df5758b5cd",
+    "url": "/static/css/2.cdaf15d1.chunk.css"
+  },
+  {
+    "revision": "6b9ba0328712cd0558d8",
+    "url": "/static/css/main.14dfa515.chunk.css"
+  },
+  {
+    "revision": "efc7751b88df5758b5cd",
+    "url": "/static/js/2.3fbf17ac.chunk.js"
+  },
+  {
+    "revision": "6b9ba0328712cd0558d8",
+    "url": "/static/js/main.f2ca88b1.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);

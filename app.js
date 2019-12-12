23352c51e3c241138d9fb12df1906d87fbd02612
Tatijana Rajcic <tatijana.rajcic@essec.edu>
2019-12-12 11:53:49 +0100

    adding cors

--- app.js
+++ app.js
@@ -13,3 +13,9 @@ app.use((req, res, next) => {
   // If no routes match, send them the React HTML.
   res.sendFile(__dirname + "/public/index.html");
 });
+
+// Seting up cors
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
